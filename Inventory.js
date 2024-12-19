8d4c5ca941908436c25068d3d5a9ae4322bb3742
anassazz <anass.azz2030@gmail.com>
2024-12-19 17:29:23 +0100

    Constructeur de la classe Inventory

--- Inventory.js
+++ Inventory.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 const Product = require('./Product');
 
 class Inventory {
+    constructor(filePath) {
+        this.filePath = filePath;
+        this.products = this.loadProducts();
+    }
 
 
 
 
 
 
-    
 }
